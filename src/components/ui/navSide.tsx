8ea31258784a2844742c893e3a0b6f885c474bf7
Thomas Machinet <thomasmachinet@gmail.com>
2024-11-04 11:44:48 +0100

    (FEAT) info restaurant

--- src/components/ui/navSide.tsx
+++ src/components/ui/navSide.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Phone, MapPin, Clock3 } from "lucide-react";
 
 export default function NavSide(props: { src: string; isOpen: boolean }) {
   console.log(props.isOpen);
@@ -32,7 +33,7 @@ export default function NavSide(props: { src: string; isOpen: boolean }) {
         </ul>
       </nav>
       <nav
-        className={`fixed flex h-full flex-col items-center justify-center gap-48 bg-primary p-10 md:flex md:w-4/12 xl:w-2/12`}
+        className={`fixed flex h-full flex-col items-center justify-center gap-32 bg-primary p-5 md:flex md:w-4/12 xl:w-2/12`}
       >
         <ul className="flex flex-col items-center gap-16 text-2xl">
           <Link href="/entrée" className="group relative">
@@ -56,10 +57,27 @@ export default function NavSide(props: { src: string; isOpen: boolean }) {
             alt="restaurantImages"
           />
         </div>
-        <ul className="flex flex-col items-center gap-16 text-2xl">
-          <li>Téléphone</li>
-          <li>Adresse</li>
-          <li>Horaires</li>
+        <ul className="flex flex-col  justify-center items-left gap-16 text-2xl">
+          <li className="flex items-center gap-5">
+            <Phone />
+            <p>09 79 34 65 96</p>
+          </li>
+          <li className="flex items-center gap-5">
+            <MapPin />
+            <div>
+              <p>40 Av. de la Gare </p>
+              <p>45340 Beaune-la-Rolande</p>
+            </div>
+          </li>
+          <li className="flex items-center gap-5">
+            <Clock3 />
+            <div className="text-left">
+              <p>Lundi au Samedi : </p>
+              <p>11:30 - 15:00</p>
+              <p>19:00 - 23:00</p>
+              <p>Fermé le Dimanche</p>
+            </div>
+          </li>
         </ul>
       </nav>
     </>
